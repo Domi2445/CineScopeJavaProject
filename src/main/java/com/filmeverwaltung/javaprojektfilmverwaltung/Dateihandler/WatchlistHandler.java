4187105c49c6a0f38d7885500b058307bc862b42
Issam <issam.abdulkarim10@gmail.com>
2026-01-01 23:57:08 +0100

    WatchlistHandler

---
+++ src/main/java/com/filmeverwaltung/javaprojektfilmverwaltung/Dateihandler/WatchlistHandler.java
@@ -0,0 +1,51 @@
+package com.filmeverwaltung.javaprojektfilmverwaltung.Dateihandler;
+
+import java.io.*;
+import java.util.List;
+
+public class WatchlistHandler extends Dateihandler {
+
+    private static final String WATCHLIST_PATH = "watchlist.json";
+
+    public WatchlistHandler() {
+        super();
+        try {
+            File file = new File(WATCHLIST_PATH);
+            if (!file.exists()) {
+                file.createNewFile();
+                try (FileWriter fw = new FileWriter(WATCHLIST_PATH)) {
+                    fw.write("[]");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> lesen() {
+        return lesen(WATCHLIST_PATH);
+    }
+
+    public void speichern(List<String> ids) {
+        speichern(ids, WATCHLIST_PATH);
+    }
+
+    public void fuegeFilmHinzu(String imdbID) {
+        List<String> liste = lesen();
+        if (!liste.contains(imdbID)) {
+            liste.add(imdbID);
+            speichern(liste);
+        }
+    }
+
+    public void entferneFilm(String imdbID) {
+        List<String> liste = lesen();
+        if (liste.remove(imdbID)) {
+            speichern(liste);
+        }
+    }
+
+    public boolean istInWatchlist(String imdbID) {
+        return lesen().contains(imdbID);
+    }
+}
